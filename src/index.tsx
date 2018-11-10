9b6ca391477e79275ee0a5a2708bcd343068376f
Koki Yamamoto <kou.197ki@gmail.com>
2018-11-11 02:28:21 +0900

    update: src/index.jsをmain.tsxに変更

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import {render} from "react-dom";
-
-const App = () => <div>Hello!</div>;
-
-render(<App/>,document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.tsx',
+    entry: './src/main.tsx',
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, './public/dist/'),
