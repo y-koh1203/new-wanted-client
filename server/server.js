b104b9f4d32e22d25b2791eb5170726de5e4f0ab
Koki Yamamoto <kou.197ki@gmail.com>
2018-11-13 20:38:04 +0900

    create: server.jsを追加

---
+++ server/server.js
@@ -0,0 +1,19 @@
+'use strict';
+const express = require('express');
+const path = require('path')
+const app = express();
+
+app.use(express.static(path.resolve(__dirname,"../public")))
+
+app.get(
+    '*',
+    (req,res) => {
+        res.sendFile(path.resolve(__dirname,"../public/index.html"));
+    });
+
+app.listen(
+    3000,
+    () => {
+        console.log('Example app listening on port 3000!');
+    }
+);
