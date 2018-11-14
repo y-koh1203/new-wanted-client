810f378a5dfdfd8f04642e65e8674356fe079130
Koki Yamamoto <kou.197ki@gmail.com>
2018-11-15 00:04:32 +0900

    update: サーバーにホットリロードを追加

--- server/server.js
+++ server/server.js
@@ -3,6 +3,16 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
+const webpack = require('webpack')
+const webpackConfig = require('./webpack.config.js')
+const compiler = webpack(webpackConfig)
+
+//ホットリロード機能
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true, publicPath: webpackConfig.output.publicPath
+}))
+app.use(require("webpack-hot-middleware")(compiler))
+
 app.use(express.static(path.resolve(__dirname,"../public")))
 
 app.get(
